966ae8090a5f0975fc8304b6980d5b1ad0a48ff7
ethan-walker <101420077+ethan-walker@users.noreply.github.com>
2024-02-19 21:44:17 +0000

    more stuff i think idk i forgot

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const generate = document.querySelector(".generate");
+const reset = document.querySelector(".reset");
 const data = document.querySelector(".sidebar-content");
+const shuffle = document.querySelector(".shuffle");
+
+shuffle.onclick = shuffleBoard;
 
 const board = document.querySelector(".board");
 
@@ -8,9 +12,18 @@ const saveStorage = (data) => localStorage.input = JSON.stringify(data);
 
 document.onkeyup = function(e) {
 	if (e.target.tagName !== "INPUT") return;
-	
+
+	generateBoard();
 	if (event.key === "Enter") {
 		e.target.blur();
+		const id = e.target.id;
+		const next = `.input-group:has(#${id}) + .input-group > input`;
+		try {
+			document.querySelector(next).focus();
+		}
+		catch {
+			// pass
+		}
 	}
 	if (event.key === "Backspace" && !e.target.value) {
 		generate.setAttribute("aria-disabled", "true")
@@ -31,6 +44,7 @@ data.querySelectorAll("input").forEach(input => {
 		}
 	}
 })
+
 function loadValues() {
 	let storage = loadStorage();
 	for (let id in storage) {
@@ -44,6 +58,7 @@ function loadValues() {
 		generate.setAttribute("aria-disabled", "true");
 	}
 }
+
 loadValues();
 function fullData() {
 	const inputs = Array.from(data.querySelectorAll("input"))
@@ -54,27 +69,28 @@ generate.onclick = generateBoard;
 
 function generateBoard() {
 	let categories = [];
-	let count = 0;
 	data.querySelectorAll("input").forEach(input => {
 		const category_title = /group-[0-9]__name/.test(input.name);
 		if (category_title) categories.push(input.value);
 		else {
+			const elem = document.querySelector(`.${input.id}`);
 			const group = input.name.match(/group-[0-9]/g)[0];
-			board.children[count].innerHTML = input.value;
-			board.children[count].classList.add(group);
-			count ++;
+			elem.innerHTML = input.value;
+			elem.classList.add(group);
 		}
 	})
-console.log(categories);
 }
-const shuffleArray = array => {
-	for (let i = array.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		const temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
-	}
+reset.onclick = function resetValues() {
+	let storage = loadStorage();
+	data.querySelectorAll("input").forEach(input => {
+		input.value = "";
+		delete storage[input.id]
+	});
+	saveStorage(storage);
+	generateBoard();
 }
+
+function shuffleArray(array) { return array.sort(() => Math.random() - 0.5) }
 
 function shuffleBoard() {
 	let board_arr = [
@@ -84,15 +100,21 @@ function shuffleBoard() {
 		[4,1], [4,2], [4,3], [4,4]
 	]
 	shuffleArray(board_arr);
-	console.log(board_arr);
-	for (let row = 0; row < 4; row++) {
-		for (let col = 0; col < 4; col++) {
-			const item = document.querySelector(`.board__item.row-${row+1}.col-${col+1}`);
-			item.classList.remove(`row-${row+1}`,`col-${col+1}`);
-			item.classList.add(`row-${board_arr[row*4+col][0]}`,`row-${board_arr[row*4+col][1]}`)
-		}
-	}
+	
+	board_arr.forEach((item, index) => {
+		const row = Math.floor(index / 4) + 1;
+		const col = (index % 4) + 1;
+		
+		const elem = document.querySelector(`.board__item.row-${row}.col-${col}:not(.moved)`);
+		
+		elem.classList.add("moved");
+		elem.classList.remove(`row-${row}`,`col-${col}`);
+		elem.classList.add(`row-${item[0]}`,`col-${item[1]}`)
+	})
+	board.querySelectorAll(".moved").forEach(item => item.classList.remove("moved"));
 }
+generateBoard();
+shuffleBoard();
 // function randomEmoji() {
 // 	content_list = ["🪅","🥥","🫚","🌺","🌾","🪻","🫛","🫠","🫕","🪸","🫗"]
 // 	for(child of board.children) {
